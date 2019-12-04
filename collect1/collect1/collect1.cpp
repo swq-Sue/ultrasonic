45bcfb73dcb77e2db0db7364b5297e61b2f142c1
swq-Sue <49257527+swq-Sue@users.noreply.github.com>
2019-12-04 20:22:43 +0800

    Add files via upload

---
+++ collect1/collect1/collect1.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <shellapi.h>
+
+int main()
+{
+
+
+	//WinExec("C:\\Users\\Administrator\\source\\repos\\ConsoleApplication11\\ConsoleApplication11\\x64\\Release\\DCMTK\\bin\\storescp.exe -aet myaet 104 +xy -fe .dcm", SW_SHOWMAXIMIZED);
+	WinExec("D:\\ultrasound\\ultrasound\\x64\\Release\\DCMTK\\bin\\storescp.exe -aet myaet -od D:\\collect1 104 +xy -fe .dcm", SW_SHOWMAXIMIZED);
+	system("pause");
+	return 0;
+
+
+
+
+
+}
